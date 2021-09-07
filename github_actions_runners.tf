62c9b397ff05fbfb2f439986974f9c4265c6ae87
SweetOps <vladimir.fess@gmail.com>
2021-09-07 15:38:51 +0300

    feat: add github-actions-runners

---
+++ github_actions_runners.tf
@@ -0,0 +1,47 @@
+locals {
+  github_actions_runners_enabled = module.this.enabled && contains(var.apps_to_install, "github_actions_runners")
+  github_actions_runners_helm_default_params = {
+    repository      = "https://sweetops.github.io/helm-charts"
+    chart           = "github-actions-runners"
+    version         = "0.0.2"
+    override_values = ""
+  }
+  github_actions_runners_helm_default_values = {
+    "fullnameOverride" = "${local.github_actions_runners["name"]}"
+  }
+  github_actions_runners = defaults(var.github_actions_runners, merge(local.helm_default_params, local.github_actions_runners_helm_default_params))
+}
+
+data "utils_deep_merge_yaml" "github_actions_runners" {
+  count = local.github_actions_runners_enabled ? 1 : 0
+
+  input = [
+    yamlencode(local.github_actions_runners_helm_default_values),
+    local.github_actions_runners["override_values"]
+  ]
+}
+
+resource "helm_release" "github_actions_runners" {
+  count = local.github_actions_runners_enabled ? 1 : 0
+
+  name              = local.github_actions_runners["name"]
+  repository        = local.github_actions_runners["repository"]
+  chart             = local.github_actions_runners["chart"]
+  version           = local.github_actions_runners["version"]
+  namespace         = local.github_actions_runners["namespace"]
+  max_history       = local.github_actions_runners["max_history"]
+  create_namespace  = local.github_actions_runners["create_namespace"]
+  dependency_update = local.github_actions_runners["dependency_update"]
+  reuse_values      = local.github_actions_runners["reuse_values"]
+  wait              = local.github_actions_runners["wait"]
+  timeout           = local.github_actions_runners["timeout"]
+  values            = [one(data.utils_deep_merge_yaml.github_actions_runners[*].output)]
+
+  depends_on = [
+    helm_release.calico,
+    helm_release.node_local_dns,
+    helm_release.cert_manager,
+    helm_release.kube_prometheus_stack,
+    helm_release.external_dns
+  ]
+}

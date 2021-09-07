89fb951ae19d2787513534d19da0a244611f31f0
SweetOps <vladimir.fess@gmail.com>
2021-09-07 15:40:51 +0300

    upd

--- github_actions_runners.tf
+++ github_actions_runners.tf
@@ -40,8 +40,6 @@ resource "helm_release" "github_actions_runners" {
   depends_on = [
     helm_release.calico,
     helm_release.node_local_dns,
-    helm_release.cert_manager,
-    helm_release.kube_prometheus_stack,
-    helm_release.external_dns
+    helm_release.actions_runner_controller
   ]
 }

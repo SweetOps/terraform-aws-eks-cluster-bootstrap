928b769c50880808e6251f50296bb26ff2a1897d
SweetOps <vladimir.fess@gmail.com>
2021-10-07 20:31:27 +0300

    upd

--- main.tf
+++ main.tf
@@ -9,7 +9,12 @@ locals {
   eks_cluster_id              = one(data.aws_eks_cluster.default[*].id)
   eks_cluster_oidc_issuer_url = one(data.aws_eks_cluster.default[*].identity[0].oidc[0].issuer)
   region                      = one(data.aws_region.default[*].name)
+  partition                   = one(data.aws_partition.default[*].partition)
   account_id                  = one(data.aws_caller_identity.default[*].account_id)
+}
+
+data "aws_partition" "default" {
+  count = module.this.enabled ? 1 : 0
 }
 
 data "aws_eks_cluster" "default" {
